888116f44c53c24c1476316e466fd0d3580817d5
LimaBD <bdl1998@hotmail.com>
2021-11-25 19:20:13 -0300

    graphics data moved to graphics.asm

--- main.asm
+++ main.asm
@@ -1,19 +1,18 @@
 ; TODO: make introduction to this file.
 
 
-; TODO: add extern functions: 'extern function'.
-global _start
 extern print_frame
 
 
+global _start
+
+
 section .data
-	; TODO: research how much large
-	;       should be the frame string.
-	%define frame_len 16
-	frame db "this is a frame.", 0
 
 
-section	text
+section	.text
+
+
 _start:
 	call presentation
 	call menu
@@ -29,9 +28,6 @@ presentation:
 
 menu:
 	; TODO: make menu.
-	mov rsi, frame
-	mov rdi, frame_len
-	call print_frame
 	ret
 
 
@@ -41,7 +37,7 @@ game:
 
 
 end:
-	xor rdi, rdi
 	mov rax, 60
+	xor rdi, rdi
     syscall
 	ret
